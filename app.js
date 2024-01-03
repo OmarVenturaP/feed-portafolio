077735298d03d04733f205ddd481e1bef73e1a8d
Omar Ventura <ventura@MacBook-Pro-de-Omar.local>
2024-01-03 10:58:43 -0600

    Creando archivo app con datos para mostrar en la vista

---
+++ app.js
@@ -0,0 +1,78 @@
+function mostrarProyectos() {
+  return {
+    proyectos: [
+      {
+        nombre: "Proyecto 1",
+        img: "https://www.simplilearn.com/ice9/free_resources_article_thumb/What_Is_a_Project.jpg",
+        descripcion:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit Perspiciatis voluptatibus similique asperiores velit, voluptas laboriosam esse nam? Quasi quos officiis quo maxime culpa nostrum optio consequuntur? Accusantium architecto nulla reiciendis!",
+      },
+      {
+        nombre: "Proyecto 2",
+        img: "https://isil.pe/blog/wp-content/uploads/2023/02/desarrollo-software-informacion.webp",
+        descripcion:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit Perspiciatis voluptatibus similique asperiores velit, voluptas laboriosam esse nam? Quasi quos officiis quo maxime culpa nostrum optio consequuntur? Accusantium architecto nulla reiciendis!",
+      },
+      {
+        nombre: "Proyecto 3",
+        img: "https://www.sydle.com/blog/assets/post/projects-and-processes-what-is-the-difference-614e00a1a9d8415db45a9230/project-and-process.jpg",
+        descripcion:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit Perspiciatis voluptatibus similique asperiores velit, voluptas laboriosam esse nam? Quasi quos officiis quo maxime culpa nostrum optio consequuntur? Accusantium architecto nulla reiciendis!",
+      },
+      {
+        nombre: "Proyecto 4",
+        img: "https://www.cambridge.org/elt/blog/wp-content/uploads/2021/04/GettyImages-1047699430-e1618396179464.jpg",
+        descripcion:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit Perspiciatis voluptatibus similique asperiores velit, voluptas laboriosam esse nam? Quasi quos officiis quo maxime culpa nostrum optio consequuntur? Accusantium architecto nulla reiciendis!",
+      },
+      {
+        nombre: "Proyecto 5",
+        img: "https://www.formate.es/wp-content/uploads/2021/06/Desarrollo-de-software-metodologias-importantes-scaled.jpg",
+        descripcion:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit Perspiciatis voluptatibus similique asperiores velit, voluptas laboriosam esse nam? Quasi quos officiis quo maxime culpa nostrum optio consequuntur? Accusantium architecto nulla reiciendis!",
+      },
+      {
+        nombre: "Proyecto 6",
+        img: "https://www.grupocibernos.com/hs-fs/hubfs/desarrollo%20de%20software%20a%20medida.jpg?width=960&name=desarrollo%20de%20software%20a%20medida.jpg",
+        descripcion:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit Perspiciatis voluptatibus similique asperiores velit, voluptas laboriosam esse nam? Quasi quos officiis quo maxime culpa nostrum optio consequuntur? Accusantium architecto nulla reiciendis!",
+      },
+      {
+        nombre: "Proyecto 7",
+        img: "https://www.techadvisor.com/wp-content/uploads/2022/06/laptop_work_garden_lifestyle_4.jpg?quality=50&strip=all",
+        descripcion:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit Perspiciatis voluptatibus similique asperiores velit, voluptas laboriosam esse nam? Quasi quos officiis quo maxime culpa nostrum optio consequuntur? Accusantium architecto nulla reiciendis!",
+      },
+      {
+        nombre: "Proyecto 8",
+        img: "https://i.rtings.com/assets/pages/KSgFV72t/best-laptops-for-working-from-home-20230503-medium.jpg",
+        descripcion:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit Perspiciatis voluptatibus similique asperiores velit, voluptas laboriosam esse nam? Quasi quos officiis quo maxime culpa nostrum optio consequuntur? Accusantium architecto nulla reiciendis!",
+      },
+      {
+        nombre: "Proyecto 9",
+        img: "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/10/10134125/Healthy-Tips-for-Working-on-Your-Laptop.jpg",
+        descripcion:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit Perspiciatis voluptatibus similique asperiores velit, voluptas laboriosam esse nam? Quasi quos officiis quo maxime culpa nostrum optio consequuntur? Accusantium architecto nulla reiciendis!",
+      },
+      {
+        nombre: "Proyecto 10",
+        img: "https://www.pcworld.com/wp-content/uploads/2023/04/hp-envy-17_notebook_home_office_a_0566-100757746-orig.jpg?quality=50&strip=all",
+        descripcion:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit Perspiciatis voluptatibus similique asperiores velit, voluptas laboriosam esse nam? Quasi quos officiis quo maxime culpa nostrum optio consequuntur? Accusantium architecto nulla reiciendis!",
+      },
+      {
+        nombre: "Proyecto 11",
+        img: "https://images.hindustantimes.com/tech/img/2021/02/10/960x540/home-office-5000278_1920_1612949547249_1612949557124.jpg",
+        descripcion:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit Perspiciatis voluptatibus similique asperiores velit, voluptas laboriosam esse nam?",
+      },
+      {
+        nombre: "Proyecto 12",
+        img: "https://www.kensington.com/siteassets/blog/2020/07-july/can-you-work-on-your-laptop-and-be-healthy-kensington-blog-meta-image.jpg",
+        descripcion:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit Perspiciatis voluptatibus similique asperiores velit, voluptas laboriosam esse nam?",
+      },
+    ],
+  };
+}
